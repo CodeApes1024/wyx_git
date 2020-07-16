abf813b837e6f1e8a217e9f57473c000e1f27a58
wangyx <3163223987@qq.com>
2020-07-16 18:21:31 +0800

    这是xx项目第一个版本提交

---
+++ src/main/java/com/bz/wyn_git/WynGitApplication.java
@@ -0,0 +1,13 @@
+package com.bz.wyn_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WynGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WynGitApplication.class, args);
+    }
+
+}
